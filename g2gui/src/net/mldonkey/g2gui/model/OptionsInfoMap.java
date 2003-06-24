f3b7377034b2672cdbd7a24727f1a264e71d0d0b
lemmstercvs01 <lemmstercvs01>
2003-06-24 09:15:27 +0000

    checkstyle applied

--- g2gui/src/net/mldonkey/g2gui/model/OptionsInfoMap.java
+++ g2gui/src/net/mldonkey/g2gui/model/OptionsInfoMap.java
@@ -31,7 +31,7 @@ import net.mldonkey.g2gui.helper.MessageBuffer;
  * OptionsInfo
  *
  * @author $user$
- * @version $Id: OptionsInfoMap.java,v 1.6 2003/06/21 13:20:36 dek Exp $ 
+ * @version $Id: OptionsInfoMap.java,v 1.7 2003/06/24 09:15:27 lemmstercvs01 Exp $ 
  *
  */
 public class OptionsInfoMap extends InfoMap {
@@ -80,7 +80,7 @@ public class OptionsInfoMap extends InfoMap {
 	 * @param value value
 	 */
 	public void update( String name, String value ) {
-		(( OptionsInfo )infoMap.get(name)).setValue(value);
+		( ( OptionsInfo )infoMap.get( name ) ).setValue( value );
 	}
 	
 	/**
@@ -99,6 +99,9 @@ public class OptionsInfoMap extends InfoMap {
 
 /*
 $Log: OptionsInfoMap.java,v $
+Revision 1.7  2003/06/24 09:15:27  lemmstercvs01
+checkstyle applied
+
 Revision 1.6  2003/06/21 13:20:36  dek
 work on optiontree continued - one can already change client_name in General-leaf
 
