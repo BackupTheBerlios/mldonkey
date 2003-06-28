dcfb937fd8177a064c46830d18a4c2b7df6bc033
mitch <mitch>
2003-06-28 11:31:56 +0000

    initial commit

---
+++ g2gui/src/net/mldonkey/g2gui/view/Tab.java
@@ -0,0 +1,60 @@
+package net.mldonkey.g2gui.view;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * @author mitch
+ *
+ * the interface every tab has to implement
+ */
+public interface Tab {
+
+	/**
+	 * initializes the Tab logically
+	 * @param preferences an object storing
+	 */
+	public void init(IPreferenceStore preferences);
+	
+	/**
+	 * used to build the tab's content
+	 * @param parent the parent Control
+	 * @return the top-level control of the tab
+	 */
+	public Control createContents(Composite parent);
+	
+	/** 
+	 * the tab's name, like "search", "transfer" etc.
+	 * @return a String containing the tab's name
+	 */
+	public String getName();
+	
+	/**
+	 * the tab's icon's name, like "search.png", "transfer.png" etc.
+	 * @return a String containing the icon's name, (w/o path)
+	 */
+	public String getIconFilename();
+	
+	/**
+	 * called when the main Application Window is closing
+	 * used to clean up the tab's data
+	 */
+	public void mainWindowClosing();
+	
+	/**
+	 * called when the "Refresh" button is selected from the Application Window Menu
+	 */
+	public void forceRefresh();
+	
+	/**
+	 * called when the tab is called to the foreground
+	 */
+	public void activated();
+	
+	/**
+	 * called when the tab is placed in the background
+	 */
+	public void deactivated();
+
+}
