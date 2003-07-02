039d52b1dfa7f7e0ef6d676abf6ad468a2bdab3b
dek <dek>
2003-07-02 17:09:28 +0000

    minor checkstyle

--- g2gui/src/net/mldonkey/g2gui/view/Tab.java
+++ g2gui/src/net/mldonkey/g2gui/view/Tab.java
@@ -18,46 +18,52 @@ public interface Tab {
 	 * @param preferences an object storing
 	 * @param core the Core-object
 	 */
-	public void init(IPreferenceStore preferences,Core core);
+	void init( IPreferenceStore preferences, Core core );
 	
 	/**
 	 * used to build the tab's content
 	 * @param parent the parent Control
 	 * @return the top-level control of the tab
 	 */
-	public Control createContents(Composite parent);
+	Control createContents( Composite parent );
 	
 	/** 
 	 * the tab's name, like "search", "transfer" etc.
 	 * @return a String containing the tab's name
 	 */
-	public String getName();
+	String getName();
 	
 	/**
 	 * the tab's icon's name, like "search.png", "transfer.png" etc.
 	 * @return a String containing the icon's name, (w/o path)
 	 */
-	public String getIconFilename();
+	String getIconFilename();
 	
 	/**
 	 * called when the main Application Window is closing
 	 * used to clean up the tab's data
 	 */
-	public void mainWindowClosing();
+	void mainWindowClosing();
 	
 	/**
 	 * called when the "Refresh" button is selected from the Application Window Menu
 	 */
-	public void forceRefresh();
+	void forceRefresh();
 	
 	/**
 	 * called when the tab is called to the foreground
 	 */
-	public void activated();
+	void activated();
 	
 	/**
 	 * called when the tab is placed in the background
 	 */
-	public void deactivated();
+	void deactivated();
 
 }
+/*
+$Log: Tab.java,v $
+Revision 1.3  2003/07/02 17:09:28  dek
+minor checkstyle
+
+*/
