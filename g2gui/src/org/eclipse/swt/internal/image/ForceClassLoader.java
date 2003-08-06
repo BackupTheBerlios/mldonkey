4c3e7ef040709796f9c3403dd64dda503d991a14
dek <dek>
2003-08-06 11:54:18 +0000

    gcj-workarounds part II

--- g2gui/src/org/eclipse/swt/internal/image/ForceClassLoader.java
+++ g2gui/src/org/eclipse/swt/internal/image/ForceClassLoader.java
@@ -1,4 +1,13 @@
 package org.eclipse.swt.internal.image;
+
+/*
+ * This file is only a file for some workarounds for gcj, don't mind if you get an error
+ * within eclipse, as this file is not used by java, only needed for static linking with gcj
+ * don't mind and please ignore ;-)
+ */
+
+import gnu.java.locale.Calendar;
+
 public class ForceClassLoader /* not part of eclipse. fixes gcj ld issue */ {
     static
     {
@@ -6,6 +15,8 @@ public class ForceClassLoader /* not part of eclipse. fixes gcj ld issue */ {
         PNGFileFormat y = new PNGFileFormat();
         JPEGFileFormat z = new JPEGFileFormat();
         WinBMPFileFormat q = new WinBMPFileFormat();
-        WinICOFileFormat p = new WinICOFileFormat();
+        WinICOFileFormat p = new WinICOFileFormat();		
+		Calendar du = new Calendar();
+		
     }
 }
