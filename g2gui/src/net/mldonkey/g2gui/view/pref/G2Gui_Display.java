d6533bb0d89564710aec88c2fe98b4f30e2ca6da
zet <zet>
2003-08-19 22:02:15 +0000

    localise

--- g2gui/src/net/mldonkey/g2gui/view/pref/G2Gui_Display.java
+++ g2gui/src/net/mldonkey/g2gui/view/pref/G2Gui_Display.java
@@ -22,6 +22,9 @@
  */
 package net.mldonkey.g2gui.view.pref;
 import java.util.ArrayList;
+
+import net.mldonkey.g2gui.view.resource.G2GuiResources;
+
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
@@ -110,29 +113,26 @@ public class G2Gui_Display extends FieldEditorPreferencePage {
 		boolean advanced = getPreferenceStore().getBoolean( "advancedMode" );
 		parent = getFieldEditorParent();
 		if ( advanced ){		
-			setupEditor( new ExtendedColorFieldEditor( "consoleBackground", "Console window background colour", parent ) );
-			setupEditor( new ExtendedColorFieldEditor( "consoleForeground", "Console window foreground colour",parent ) );
-			setupEditor( new ExtendedColorFieldEditor( "consoleHighlight", "Console window highlight colour", parent ) );
-			setupEditor( new ExtendedColorFieldEditor( "consoleInputBackground", "Console input background colour", parent ) );
-			setupEditor( new ExtendedColorFieldEditor( "consoleInputForeground", "Console input foreground colour", parent ) );
-			setupEditor( new ExtendedFontFieldEditor2( "consoleFontData", "Console window font", "Sample", parent ) );
-		}	
-		
-		if ( advanced )
-			setupEditor( new BooleanFieldEditor( "displayAllServers", "Show only connected servers", parent ) );
+			setupEditor( new ExtendedColorFieldEditor( "consoleBackground", G2GuiResources.getString("PREF_DISPLAY_CONSOLE_OBK"), parent ) );
+			setupEditor( new ExtendedColorFieldEditor( "consoleForeground", G2GuiResources.getString("PREF_DISPLAY_CONSOLE_OFG"),parent ) );
+			setupEditor( new ExtendedColorFieldEditor( "consoleHighlight",  G2GuiResources.getString("PREF_DISPLAY_CONSOLE_HL"), parent ) );
+			setupEditor( new ExtendedColorFieldEditor( "consoleInputBackground", G2GuiResources.getString("PREF_DISPLAY_CONSOLE_IBK"), parent ) );
+			setupEditor( new ExtendedColorFieldEditor( "consoleInputForeground", G2GuiResources.getString("PREF_DISPLAY_CONSOLE_IFG"), parent ) );
+			setupEditor( new ExtendedFontFieldEditor2( "consoleFontData", G2GuiResources.getString("PREF_DISPLAY_CONSOLE_FONT"), "Sample", parent ) );
+			setupEditor( new BooleanFieldEditor( "displayAllServers", G2GuiResources.getString("PREF_DISPLAY_CONN_SERV"), parent ) );
+		}			
+																								
+		setupEditor( new BooleanFieldEditor( "displayHeaderBar", G2GuiResources.getString("PREF_DISPLAY_HEADER"), parent ) );
 				
-		setupEditor( new BooleanFieldEditor( "displayHeaderBar", "Display header bar", parent ) );
+		setupEditor( new BooleanFieldEditor(  "displayChunkGraphs", G2GuiResources.getString("PREF_DISPLAY_CHUNK"), parent ) );
 				
-		setupEditor( new BooleanFieldEditor(  "displayChunkGraphs", "Display chunk graphs", parent ) );
+		setupEditor( new BooleanFieldEditor( "displayGridLines", G2GuiResources.getString("PREF_DISPLAY_GRID"), parent ) );
 				
-		setupEditor( new BooleanFieldEditor( "displayGridLines", "Display grid lines", parent ) );
+		setupEditor( new BooleanFieldEditor( "tableCellEditors", G2GuiResources.getString("PREF_DISPLAY_CELLEDITORS"), parent ) );
 				
-		setupEditor( new BooleanFieldEditor( "tableCellEditors", "Activate table cell editors", parent ) );
+		setupEditor( new BooleanFieldEditor( "forceRefresh", G2GuiResources.getString("PREF_DISPLAY_FULL_REFRESH"),parent ) );
 				
-		setupEditor( new BooleanFieldEditor( "forceRefresh", "Force full table refresh at each update ( maintains sort order, might flicker )",parent ) );
-				
-		IntegerFieldEditor displayBuffer = new IntegerFieldEditor( "displayBuffer", "GUI update buffer ( 0-60 seconds )",parent );
-				
+		IntegerFieldEditor displayBuffer = new IntegerFieldEditor( "displayBuffer", G2GuiResources.getString("PREF_DISPLAY_UPDATE_BUFFER"),parent );
 		displayBuffer.setValidRange( 0, 60 );
 		setupEditor( displayBuffer );
 		arrangeFields();
@@ -140,6 +140,9 @@ public class G2Gui_Display extends FieldEditorPreferencePage {
 }
 /*
 $Log: G2Gui_Display.java,v $
+Revision 1.15  2003/08/19 22:02:15  zet
+localise
+
 Revision 1.14  2003/08/19 13:16:10  dek
 advanced-Mode introduced
 
