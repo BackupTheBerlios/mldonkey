7666d81a2ac327faea732cae7b8c2eb0e7169df0
zet <zet>
2003-09-16 02:12:07 +0000

    initial

---
+++ g2gui/src/net/mldonkey/g2gui/view/helper/HeaderBarMouseAdapter.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2003
+ * G2Gui Team
+ * 
+ * 
+ * This file is part of G2Gui.
+ *
+ * G2Gui is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * G2Gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with G2Gui; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+package net.mldonkey.g2gui.view.helper;
+
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Menu;
+
+
+/**
+ * HeaderBarMouseAdapter - A helper for header bar mouse events & menus
+ *
+ * @version $Id: HeaderBarMouseAdapter.java,v 1.1 2003/09/16 02:12:07 zet Exp $ 
+ *
+ */
+public class HeaderBarMouseAdapter extends MouseAdapter {
+
+	CLabel cLabel;
+	MenuManager menuManager;
+
+	public HeaderBarMouseAdapter(CLabel cLabel, MenuManager menuManager) {
+		this.cLabel = cLabel;
+		this.menuManager = menuManager;
+	}
+	private boolean overImage(int x) {
+		return x < cLabel.getImage().getBounds().width;
+	}
+	private void showMenu(Point p) {
+		Menu menu = menuManager.createContextMenu(cLabel);
+		menu.setLocation(p);
+		menu.setVisible(true);
+	}
+	public void mouseDown(MouseEvent e) {
+		if ((e.button == 1 && overImage(e.x)) || e.button == 3) {
+			Point p;
+			if (e.button == 1) {
+				p = new Point(0, cLabel.getBounds().height);
+			} else {
+				p = new Point(e.x, e.y);
+			}
+			showMenu(((CLabel) e.widget).toDisplay(p));
+		}
+	}
+}
+
+/*
+$Log: HeaderBarMouseAdapter.java,v $
+Revision 1.1  2003/09/16 02:12:07  zet
+initial
+
+
+*/
