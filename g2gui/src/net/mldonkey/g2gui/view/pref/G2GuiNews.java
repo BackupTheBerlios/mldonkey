04654f599498b5eec680adebaa12b1a01d5cf37e
lemmster <lemmster>
2003-10-01 20:56:50 +0000

    initial commit (empty for the moment)

---
+++ g2gui/src/net/mldonkey/g2gui/view/pref/G2GuiNews.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2003
+ * G2Gui Team
+ *
+ *
+ * This file is part of G2Gui.
+ *
+ * G2Gui is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * G2Gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with G2Gui; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package net.mldonkey.g2gui.view.pref;
+
+
+/**
+ * G2Gui_News
+ *
+ * @version $Id: G2GuiNews.java,v 1.1 2003/10/01 20:56:50 lemmster Exp $
+ *
+ */
+public class G2GuiNews extends PreferencePage {
+    /**
+     * @param title
+     * @param style
+     */
+    protected G2GuiNews( String title, int style ) {
+        super( title, style );
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors()
+     */
+    protected void createFieldEditors() {
+        // TODO Auto-generated method stub
+    }
+}
+
+/*
+$Log: G2GuiNews.java,v $
+Revision 1.1  2003/10/01 20:56:50  lemmster
+initial commit (empty for the moment)
+
+*/
