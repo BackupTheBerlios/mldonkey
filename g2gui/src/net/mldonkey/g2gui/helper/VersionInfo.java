38c7276b63125e653e9c1558b41df4a6c0ec2749
vnc <vnc>
2003-11-04 16:16:55 +0000

    little version update

--- g2gui/src/net/mldonkey/g2gui/helper/VersionInfo.java
+++ g2gui/src/net/mldonkey/g2gui/helper/VersionInfo.java
@@ -25,7 +25,7 @@ package net.mldonkey.g2gui.helper;
 /**
  * VersionInfo returns just a String with our current g2gui version number.
  *
- * @version $Id: VersionInfo.java,v 1.1 2003/10/09 09:59:19 lemmster Exp $ 
+ * @version $Id: VersionInfo.java,v 1.2 2003/11/04 16:16:55 vnc Exp $ 
  *
  */
 public class VersionInfo {
@@ -33,12 +33,15 @@ public class VersionInfo {
 	 * @return Our version number
 	 */
 	public static String getVersion() {
-		return "0.1.1";
+		return "0.1.1+ CVS";
 	}
 }
 
 /*
 $Log: VersionInfo.java,v $
+Revision 1.2  2003/11/04 16:16:55  vnc
+little version update
+
 Revision 1.1  2003/10/09 09:59:19  lemmster
 get the version number from the centralized class VersionInfo
 
