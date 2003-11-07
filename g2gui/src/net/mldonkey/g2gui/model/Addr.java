6208297e4fd3bcdec331ca6c243c27b9f3e619f1
zet <zet>
2003-11-07 15:12:14 +0000

    fix log

--- g2gui/src/net/mldonkey/g2gui/model/Addr.java
+++ g2gui/src/net/mldonkey/g2gui/model/Addr.java
@@ -31,7 +31,7 @@ import net.mldonkey.g2gui.helper.MessageBuffer;
  * Addr
  * 
  *
- * @version $Id: Addr.java,v 1.14 2003/10/20 19:30:13 lemmster Exp $
+ * @version $Id: Addr.java,v 1.15 2003/11/07 15:12:14 zet Exp $
  */
 public class Addr implements SimpleInformation {
 	/**
@@ -134,47 +134,49 @@ public class Addr implements SimpleInformation {
 	}
 }
 /*
-$$Log: Addr.java,v $
-$Revision 1.14  2003/10/20 19:30:13  lemmster
-$fixed Bug #1006
-$
-$Revision 1.13  2003/09/02 09:24:36  lemmster
-$checkstyle
-$
-$Revision 1.12  2003/08/23 15:21:37  zet
-$remove @author
-$
-$Revision 1.11  2003/08/22 21:03:15  lemmster
-$replace $user$ with $Author: lemmster $
-$
-$Revision 1.10  2003/08/11 11:23:06  lemmstercvs01
-$fix sort by string
-$
-$Revision 1.9  2003/08/09 13:42:24  dek
-$added gnu.regexp for compiling with gcj
-$you can get it at:
-$
-$ftp://ftp.tralfamadore.com/pub/java/gnu.regexp-1.1.4.tar.gz
-$
-$Revision 1.8  2003/08/06 17:36:06  lemmstercvs01
-$save a string address as string and not as InetAddress anymore (speed!!)
-$
-$Revision 1.7  2003/07/30 19:27:49  lemmstercvs01
-$address is always an InetAddress instead of just a String
-$
-$Revision 1.6  2003/07/05 20:04:10  lemmstercvs01
-$javadoc improved
-$
-$Revision 1.5  2003/07/04 10:26:03  lemmstercvs01
-$minor: just checkstyle
-$
-$Revision 1.4  2003/06/30 07:21:27  lemmstercvs01
-$changed ip from int to InetAddress
-$
-$Revision 1.3  2003/06/18 13:30:56  dek
-$Improved Communication Layer view <--> model by introducing a super-interface
-$
-$Revision 1.2  2003/06/14 20:30:44  lemmstercvs01
-$cosmetic changes
-$$
+$Log: Addr.java,v $
+Revision 1.15  2003/11/07 15:12:14  zet
+fix log
+
+Revision 1.14  2003/10/20 19:30:13  lemmster
+fixed Bug #1006
+
+Revision 1.13  2003/09/02 09:24:36  lemmster
+checkstyle
+
+Revision 1.12  2003/08/23 15:21:37  zet
+remove @author
+
+Revision 1.11  2003/08/22 21:03:15  lemmster
+replace user with Author: lemmster 
+
+Revision 1.10  2003/08/11 11:23:06  lemmstercvs01
+fix sort by string
+
+Revision 1.9  2003/08/09 13:42:24  dek
+added gnu.regexp for compiling with gcj
+you can get it at:
+
+ftp://ftp.tralfamadore.com/pub/java/gnu.regexp-1.1.4.tar.gz
+
+Revision 1.8  2003/08/06 17:36:06  lemmstercvs01
+save a string address as string and not as InetAddress anymore (speed!!)
+
+Revision 1.7  2003/07/30 19:27:49  lemmstercvs01
+address is always an InetAddress instead of just a String
+
+Revision 1.6  2003/07/05 20:04:10  lemmstercvs01
+javadoc improved
+
+Revision 1.5  2003/07/04 10:26:03  lemmstercvs01
+minor: just checkstyle
+
+Revision 1.4  2003/06/30 07:21:27  lemmstercvs01
+changed ip from int to InetAddress
+
+Revision 1.3  2003/06/18 13:30:56  dek
+Improved Communication Layer view <--> model by introducing a super-interface
+
+Revision 1.2  2003/06/14 20:30:44  lemmstercvs01
+cosmetic changes
 */
