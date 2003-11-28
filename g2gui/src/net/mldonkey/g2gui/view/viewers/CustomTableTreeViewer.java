7ef184484000dcc161f3b48a3f04eec7e45beb5a
zet <zet>
2003-11-28 20:51:52 +0000

    small method move

--- g2gui/src/net/mldonkey/g2gui/view/viewers/CustomTableTreeViewer.java
+++ g2gui/src/net/mldonkey/g2gui/view/viewers/CustomTableTreeViewer.java
@@ -40,6 +40,7 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Item;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Widget;
 
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -51,7 +52,7 @@ import java.util.List;
  * CustomTableTreeViewer
  *
  *
- * @version $Id: CustomTableTreeViewer.java,v 1.4 2003/11/23 17:58:03 lemmster Exp $
+ * @version $Id: CustomTableTreeViewer.java,v 1.5 2003/11/28 20:51:52 zet Exp $
  *
  */
 public class CustomTableTreeViewer extends TableTreeViewer implements ICustomViewer {
@@ -108,18 +109,16 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
             TableTreeItem[] children = parents[ i ].getItems();
 
             for (int j = 0; j < children.length; j++) {
-                if (children[ j ].getData() != null) {
+                if (children[ j ].getData() != null)
                     associateTTE(children[ j ].getData(), children[ j ], true);
-                }
             }
         }
     }
 
     private void disposeTTE(CTableTreeEditor tableTreeEditor) {
         if (tableTreeEditor != null) {
-            if (tableTreeEditor.getEditor() != null) {
+            if (tableTreeEditor.getEditor() != null)
                 tableTreeEditor.getEditor().dispose();
-            }
 
             tableTreeEditor.dispose();
         }
@@ -128,9 +127,8 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
     public void remove(Object[] elements) {
         super.remove(elements);
 
-        if (activeEditors) {
+        if (activeEditors)
             removeTTE(elements);
-        }
     }
 
     public void removeTTE(Object[] elements) {
@@ -147,23 +145,15 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
         nudgeColumn();
     }
 
-    protected void associate(Object element, Item item) {
-        if (activeEditors) {
-            associateTTE(element, item, false);
-        }
-
-        super.associate(element, item);
-    }
-
     private void associateTTE(Object element, Item item, boolean forceUpdate) {
         if ((item.getData() != element) || forceUpdate) {
             if (!tableTreeEditors.containsKey(element)) {
                 FileInfo fileInfo = null;
                 ClientInfo clientInfo = null;
 
-                if (element instanceof FileInfo) {
+                if (element instanceof FileInfo)
                     fileInfo = (FileInfo) element;
-                } else if (element instanceof TreeClientInfo) {
+                else if (element instanceof TreeClientInfo) {
                     TreeClientInfo treeClientInfo = (TreeClientInfo) element;
                     fileInfo = treeClientInfo.getFileInfo();
                     clientInfo = treeClientInfo.getClientInfo();
@@ -173,14 +163,14 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
                 tableTreeEditor.horizontalAlignment = SWT.LEFT;
                 tableTreeEditor.grabHorizontal = true;
 
-                ChunkCanvas chunkCanvas = new ChunkCanvas(this.getTableTree().getTable(), SWT.NO_BACKGROUND, clientInfo, fileInfo, null);
+                ChunkCanvas chunkCanvas = new ChunkCanvas(this.getTableTree().getTable(),
+                        SWT.NO_BACKGROUND, clientInfo, fileInfo, null);
                 tableTreeEditor.setEditor(chunkCanvas, (TableTreeItem) item, chunksColumn);
 
-                if (clientInfo != null) {
+                if (clientInfo != null)
                     clientInfo.addObserver(chunkCanvas);
-                } else {
+                else
                     fileInfo.addObserver(chunkCanvas);
-                }
 
                 tableTreeEditors.put(element, tableTreeEditor);
             } else {
@@ -190,34 +180,38 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
         }
     }
 
-    protected void disassociate(Item item) {
-        if (activeEditors) {
-            disassociateTTE(item);
-        }
-
-        super.disassociate(item);
+    protected void mapElement(Object element, Widget item) {
+        super.mapElement(element, item);
+        if (activeEditors) 
+            associateTTE(element, (Item) item, true);
     }
 
-    private void disassociateTTE(Item item) {
-        // Fully dispose if filters are active, or processing child items
-        if ((getFilters().length > 0) || item.getData() instanceof TreeClientInfo) {
-            disposeTTE((CTableTreeEditor) tableTreeEditors.get(item.getData()));
-            tableTreeEditors.remove(item.getData());
-        } else {
-            CTableTreeEditor tableTreeEditor = (CTableTreeEditor) tableTreeEditors.get(item.getData());
+    protected void unmapElement(Object element) {
+        super.unmapElement(element);
+        if (activeEditors)
+            disassociateTTE(element);
+    }
 
-            if (tableTreeEditor != null) {
-                tableTreeEditor.setItem(null);
-            }
-        }
+    private void disassociateTTE(Object element) {
+        // Fully dispose if filters are active, or processing child items
+        //  if ((getFilters().length > 0) || item.getData() instanceof TreeClientInfo) {
+        
+        disposeTTE((CTableTreeEditor) tableTreeEditors.get(element));
+        tableTreeEditors.remove(element);
+
+        //        } else {
+        //            CTableTreeEditor tableTreeEditor = (CTableTreeEditor) tableTreeEditors.get(element);
+        //
+        //            if (tableTreeEditor != null)
+        //                tableTreeEditor.setItem(null);
+        //        }
     }
 
     protected void internalRefresh(Object element, boolean updateLabels) {
         super.internalRefresh(element, updateLabels);
 
-        if (activeEditors) {
+        if (activeEditors)
             nudgeColumn();
-        }
     }
 
     public void expandAll() {
@@ -229,24 +223,23 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
         super.collapseAll();
         nudgeColumn();
     }
-    
+
     public void collapseToLevel(Object element, int level) {
         super.collapseToLevel(element, level);
         nudgeColumn();
     }
-    
-	public void expandToLevel(Object element, int level) {
-	    super.expandToLevel(element, level);
-	    nudgeColumn();
-	}
-    
+
+    public void expandToLevel(Object element, int level) {
+        super.expandToLevel(element, level);
+        nudgeColumn();
+    }
+
     // End TTE hack	
     public void setColumnIDs(String string) {
         columnIDs = new int[ string.length() ];
 
-        for (int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < string.length(); i++)
             columnIDs[ i ] = string.charAt(i) - ColumnSelector.MAGIC_NUMBER;
-        }
     }
 
     public int[] getColumnIDs() {
@@ -264,11 +257,10 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
         ITableLabelProvider tprov = null;
         ILabelProvider lprov = null;
 
-        if (prov instanceof ITableLabelProvider) {
+        if (prov instanceof ITableLabelProvider)
             tprov = (ITableLabelProvider) prov;
-        } else {
+        else
             lprov = (ILabelProvider) prov;
-        }
 
         int columnCount = this.getTableTree().getTable().getColumnCount();
         TableTreeItem ti = (TableTreeItem) item;
@@ -289,36 +281,38 @@ public class CustomTableTreeViewer extends TableTreeViewer implements ICustomVie
             }
 
             // Only set text if it changes
-            if (!text.equals(ti.getText(column))) {
+            if (!text.equals(ti.getText(column)))
                 ti.setText(column, text);
-            }
 
             // Apparently a problem to setImage to null if already null
-            if (ti.getImage(column) != image) {
+            if (ti.getImage(column) != image)
                 ti.setImage(column, image);
-            }
         }
 
         if (prov instanceof IColorProvider) {
             IColorProvider cprov = (IColorProvider) prov;
 
             if (ti.getForeground() != cprov.getForeground(element)) {
-                if ((cprov.getForeground(element) != null) || (!ti.getParent().getForeground().getRGB().equals(ti.getForeground().getRGB()))) {
+                if ((cprov.getForeground(element) != null) ||
+                        (!ti.getParent().getForeground().getRGB().equals(ti.getForeground().getRGB())))
                     ti.setForeground(cprov.getForeground(element));
-                }
             }
 
             if (ti.getBackground() != cprov.getBackground(element)) {
-                if ((cprov.getBackground(element) != null) || (!ti.getParent().getBackground().getRGB().equals(ti.getBackground().getRGB()))) {
+                if ((cprov.getBackground(element) != null) ||
+                        (!ti.getParent().getBackground().getRGB().equals(ti.getBackground().getRGB())))
                     ti.setBackground(cprov.getBackground(element));
-                }
             }
         }
     }
 }
 
+
 /*
 $Log: CustomTableTreeViewer.java,v $
+Revision 1.5  2003/11/28 20:51:52  zet
+small method move
+
 Revision 1.4  2003/11/23 17:58:03  lemmster
 removed dead/unused code
 
