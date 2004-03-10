7efb1087417103997df57503ee582a98b17c3f13
dek <dek>
2004-03-10 10:31:44 +0000

    added header and footer

--- g2gui/src/net/mldonkey/g2gui/view/systray/SystemTray.java
+++ g2gui/src/net/mldonkey/g2gui/view/systray/SystemTray.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2003
+ * g2gui Team
+ * 
+ * 
+ * This file is part of g2gui.
+ *
+ * g2gui is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * g2gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with g2gui; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
 package net.mldonkey.g2gui.view.systray;
 
 import java.text.DecimalFormat;
@@ -19,8 +41,9 @@ import org.eclipse.swt.widgets.Shell;
 import com.gc.systray.SystemTrayIconListener;
 import com.gc.systray.SystemTrayIconManager;
 
-/**
- * Test class
+ /**
+ * @version $Id: SystemTray.java,v 1.7 2004/03/10 10:31:44 dek Exp $ 
+ *
  */
 public class SystemTray implements SystemTrayIconListener, Runnable,Observer {
 
@@ -211,3 +234,9 @@ public class SystemTray implements SystemTrayIconListener, Runnable,Observer {
 
 
 }
+/*
+ $Log: SystemTray.java,v $
+ Revision 1.7  2004/03/10 10:31:44  dek
+ added header and footer
+
+ */
--- g2gui/src/net/mldonkey/g2gui/view/systray/ToggleAction.java
+++ g2gui/src/net/mldonkey/g2gui/view/systray/ToggleAction.java
@@ -1,8 +1,24 @@
 /*
- * Created on 09.03.2004
+ * Copyright 2003
+ * g2gui Team
+ * 
+ * 
+ * This file is part of g2gui.
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * g2gui is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * g2gui is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with g2gui; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
  */
 package net.mldonkey.g2gui.view.systray;
 
@@ -11,11 +27,9 @@ import net.mldonkey.g2gui.view.MainWindow;
 
 
 
-/**
- * @author joerg
- * 
- * To change the template for this generated type comment go to Window -
- * Preferences - Java - Code Generation - Code and Comments
+ /**
+ * @version $Id: ToggleAction.java,v 1.3 2004/03/10 10:31:44 dek Exp $ 
+ *
  */
 class ToggleAction extends SystrayAction {
 	
@@ -36,3 +50,9 @@ class ToggleAction extends SystrayAction {
 	}
 
 }
+/*
+ $Log: ToggleAction.java,v $
+ Revision 1.3  2004/03/10 10:31:44  dek
+ added header and footer
+
+ */
